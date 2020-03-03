d6a31fa765742fa68902d368f905c8fc19f5cd8c
Tony Duco <nosduco@gmail.com>
2020-03-03 13:02:04 -0500

    Patch to work on patched mojave instances

--- Library/Homebrew/extend/os/mac/hardware.rb
+++ Library/Homebrew/extend/os/mac/hardware.rb
@@ -2,10 +2,6 @@
 
 module Hardware
   def self.oldest_cpu(version = MacOS.version)
-    if version >= :mojave
-      :nehalem
-    else
-      generic_oldest_cpu
-    end
+     generic_oldest_cpu
   end
 end
